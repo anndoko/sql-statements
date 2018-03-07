c62f80ec7214af4004a5d35f85834c40ddac8ba6
Deahan <deahanyu@umich.edu>
2018-03-07 17:43:41 -0500

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,27 @@
+'''
+SI 505/206 W18 homework 9: Basic SQL statements
+'''
+
+import sqlite3 as sqlite
+
+#----- Q1. Show all rows from the Region table 
+
+#----- Q2. How many customers are there? 
+
+#----- Q3. How many orders have been made? 
+
+#----- Q4. Show the first five rows from the Product table 
+
+#----- Q5. Show all available categories 
+
+#----- Q6. Show the five cheapest products 
+
+#----- Q7. Show all products that have more than 100 units in stock 
+
+#----- Q8. Show all columns in the Order table 
+
+#----- Q9. Identify each employee's first name and the number of order each employee has made. Sort them by the total number of orders in decreasing order
+
+#----- Q10. Identify the products and the corresponding supply companies in Ann Arbor 
+
+
