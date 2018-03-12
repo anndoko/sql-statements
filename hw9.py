7664bc0b5f061374b518dc92d8b24cfdaab30aba
Anndo Ko <handcuffanndo@gmail.com>
2018-03-12 16:10:52 -0400

    format the output

--- hw9.py
+++ hw9.py
@@ -11,40 +11,42 @@ cur = conn.cursor()
 statement1 = "SELECT * "
 statement1 += "FROM Region;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Rows in the Region Table\n")
+print("Id | RegionDescription")
+print("-" * len("Id | RegionDescription"))
 
 rows_in_region_table = cur.execute(statement1)
 for row in rows_in_region_table:
     (Id, RegionDescription) = row
-    print(Id, RegionDescription)
+    print("%s  | %s"%(Id, RegionDescription))
 
 #----- Q2. How many customers are there?
 statement2 = "SELECT COUNT(Id) "
 statement2 += "FROM Customer;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("The Number of Customers\n")
 
 num_of_customers = cur.execute(statement2)
-print(num_of_customers.fetchone()[0])
+print(num_of_customers.fetchone()[0], "customers")
 
 #----- Q3. How many orders have been made?
 statement3 = "SELECT COUNT(Id) "
 statement3 += "FROM [Order];"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("The Number of Orders\n")
 
 num_of_orders = cur.execute(statement3)
-print(num_of_orders.fetchone()[0])
+print(num_of_orders.fetchone()[0], "orders")
 
 #----- Q4. Show the first five rows from the Product table
 statement4 = "SELECT * "
 statement4 += "FROM Product "
 statement4 += "LIMIT 5;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Top 5 Rows in the Product Table\n")
 
 top_five_rows = cur.execute(statement4)
@@ -56,7 +58,7 @@ for row in top_five_rows:
 statement5 = "SELECT CategoryName "
 statement5 += "FROM Category;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Available Categories\n")
 
 available_categories = cur.execute(statement5)
@@ -70,7 +72,7 @@ statement6 += "FROM Product "
 statement6 += "ORDER BY UnitPrice ASC "
 statement6 += "LIMIT 5;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Top 5 Cheapest Products\n")
 
 cheapes_products = cur.execute(statement6)
@@ -83,13 +85,13 @@ statement7 = "SELECT ProductName, UnitsInStock "
 statement7 += "FROM Product "
 statement7 += "WHERE UnitsInStock > 100;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Products (>100 Units in Stock)\n")
 
 products_more_than_100_units_in_stock = cur.execute(statement7)
 for row in products_more_than_100_units_in_stock:
     (ProductName, UnitsInStock) = row
-    print(ProductName, UnitsInStock)
+    print(ProductName + ": ", UnitsInStock)
 
 
 #----- Q8. Show all columns in the Order table
@@ -97,7 +99,7 @@ statement8 = "SELECT * "
 statement8 += "FROM [Order]"
 statement8 += "LIMIT 1;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Columns in the Order Table\n")
 
 columns_in_order_table = cur.execute(statement8)
@@ -111,13 +113,13 @@ statement9 += "ON [Order].EmployeeId = Employee.Id "
 statement9 += "GROUP BY EmployeeId "
 statement9 += "ORDER BY OrderCount DESC;"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("The Number of Orders Each Employee Made\n")
 
 num_of_orders_employee = cur.execute(statement9)
 for row in num_of_orders_employee:
     (NumOfOrder, LastName, FirstName) = row
-    print(LastName, FirstName, NumOfOrder)
+    print(FirstName + ": ", NumOfOrder)
 
 #----- Q10. Identify the products and the corresponding supply companies in Ann Arbor
 statement10 = "SELECT p.ProductName, s.CompanyName "
@@ -125,10 +127,10 @@ statement10 += "FROM Product AS p INNER JOIN Supplier AS s "
 statement10 += "ON p.SupplierId = s.Id "
 statement10 += "WHERE s.City = 'Ann Arbor'"
 
-print("\n" + "--" * 20)
+print("\n" + "=" * 30)
 print("Products Made by Supply Companies in Ann Arbor\n")
 
 product_company_in_ann_arbor = cur.execute(statement10)
 for row in product_company_in_ann_arbor:
     (ProductName, CompanyName) = row
-    print(ProductName, CompanyName)
+    print("%s, %s"%(ProductName, CompanyName))
